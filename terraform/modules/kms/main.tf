fa2abe01ad2ead93e1f501c8b7684179b45084ab
Ricardino Guarnieri <nieri0x73@users.noreply.github.com>
2026-05-09 22:01:38 -0300

    fix(vault): embed ocikms seal config directly in standalone.config

--- terraform/modules/kms/main.tf
+++ terraform/modules/kms/main.tf
@@ -32,20 +32,3 @@ resource "oci_identity_policy" "vault_kms_unseal" {
     "Allow dynamic-group ${oci_identity_dynamic_group.oke_nodes.name} to use keys in compartment id ${var.compartment_id} where target.key.id = '${oci_kms_key.vault_unseal.id}'"
   ]
 }
-
-resource "local_file" "vault_kms_secret" {
-  filename        = "${path.module}/../../../gitops/config/vault/vault-kms-secret.yaml"
-  file_permission = "0644"
-  content         = <<-EOT
-    apiVersion: v1
-    kind: Secret
-    metadata:
-      name: vault-kms-secret
-      namespace: security
-    type: Opaque
-    stringData:
-      VAULT_SEAL_KEY_ID: "${oci_kms_key.vault_unseal.id}"
-      VAULT_SEAL_CRYPTO_ENDPOINT: "${oci_kms_vault.this.crypto_endpoint}"
-      VAULT_SEAL_MANAGEMENT_ENDPOINT: "${oci_kms_vault.this.management_endpoint}"
-  EOT
-}
