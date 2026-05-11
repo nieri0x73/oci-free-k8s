c1a87e7d5beadbc77e7a2a951a9a8ecf5aab82f2
Ricardino Guarnieri <nieri0x73@users.noreply.github.com>
2026-05-11 19:00:33 -0300

    chore(terraform): centralize provider constraints in root, upgrade oci to 8.13

--- terraform/modules/oke/terraform.tf
+++ terraform/modules/oke/terraform.tf
@@ -1,16 +1,10 @@
 terraform {
   required_providers {
-    oci = {
-      source  = "oracle/oci"
-      version = "~> 8.13.0"
-    }
     jq = {
-      source  = "massdriver-cloud/jq"
-      version = "0.2.1"
+      source = "massdriver-cloud/jq"
     }
     local = {
-      source  = "hashicorp/local"
-      version = "~> 2.8"
+      source = "hashicorp/local"
     }
   }
 }
